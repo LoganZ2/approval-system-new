66c352b1f3d50fcf24639fa1d2344371c3ba633d
LoganZ2 <1540961836@qq.com>
2026-01-01 19:35:53 +0800

    test wx

--- src/leave/controllers/leave.controller.ts
+++ src/leave/controllers/leave.controller.ts
@@ -10,10 +10,19 @@ import {
 } from '@nestjs/common';
 import { LeaveService } from '../services/leave.service';
 import { Application, PendingApproval } from '../types';
+import { post, request } from 'axios';
+import * as cloud from "wx-server-sdk";
+
+
 
 @Controller('leave')
 export class LeaveController {
-  constructor(private readonly leaveService: LeaveService) {}
+  constructor(private readonly leaveService: LeaveService) {
+    cloud.init({
+      env: 'prod-0gov9rdc5eed3c97',
+      traceUser: true
+    })
+  }
 
   @Get("/applications")
   async selectApplicationByUserId(@Query("userId") userId: Number): Promise<Application[]> {
@@ -25,5 +34,13 @@ export class LeaveController {
   async selectPendingApprovalByUserId(@Query("userId") userId: Number): Promise<PendingApproval[]> {
     const result = await this.leaveService.selectPendingApprovalByUserId(userId);
     return result;
+  }
+
+
+
+  @Get("/test-send")
+  async testSend() {
+    const context = cloud.getWXContext()
+    return JSON.stringify(context)
   }
 }
