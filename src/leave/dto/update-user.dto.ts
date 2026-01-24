106b9427317a77a5a51f16889b0ca792b673c19e
LoganZ2 <1540961836@qq.com>
2026-01-25 00:37:41 +0800

    validator fix

--- src/leave/dto/update-user.dto.ts
+++ src/leave/dto/update-user.dto.ts
@@ -6,11 +6,10 @@ export class UpdateUserDto {
   id: number;
 
   @IsString()
-  @MinLength(2, { message: 'Name must be at least 2 characters long' })
+  @MinLength(1, { message: '名字不能为空' })
   name: string;
 
   @IsString()
-  @MinLength(2, { message: 'Department must be at least 2 characters long' })
   department: string;
 
   @IsEnum(Level)
